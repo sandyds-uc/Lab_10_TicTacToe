b1803c89ac9aa79e1b75d0b792fe3395a4c9c230
Daniel Sandy <sandyds@mail.uc.edu>
2024-07-02 23:57:02 -0400

    Initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,61 @@
+
+/**
+ *
+ * @author Daniel Sandy sandyds@mail.uc.edu
+ */
+
+public class TicTacToe
+{
+
+    private static final int ROW = 3; // declare & initialize global constant ROW
+    private static final int COL = 3; // declare & initialize global constant COL
+    private static String board[][] = new String[ROW][COL];// declare & initialize global variable board
+
+    public static void main(String[] args)
+    {
+
+    } // return main()
+
+    // methods to use in this program
+
+    private static void clearBoard()
+    {
+
+    }
+
+    private static void display()
+    {
+
+    }
+
+    private static boolean isValidMove()
+    {
+        return true;
+    }
+
+    private static boolean isWin()
+    {
+        return true;
+    }
+
+    private static boolean isColWin()
+    {
+        return true;
+    }
+
+    private static boolean isRowWin()
+    {
+        return true;
+    }
+
+    private static boolean isDiagonalWin()
+    {
+        return true;
+    }
+
+    private static boolean isTie()
+    {
+        return true;
+    }
+
+} // end class TicTacToe
